d2dc80b09a509c0cc01e0e1124b6d84d3eeb3ca9
Mario Riascos <marioriascos1201@gmail.com>
2023-03-04 22:38:34 -0500

    Update Template

--- __manifest__.py
+++ __manifest__.py
@@ -18,18 +18,40 @@
 #
 ##############################################################################
 
+# -*- coding: utf-8 -*-
 {
-    'name': 'Father MFH',
-    'version': '16.0.1.0.0',
-    'author': 'Mario Fernando Riascos',
-    'maintainer': 'Mario Fernando Riascos',
-    'website': 'mfriascos.github.io',
-    'license': 'AGPL-3',
-    'category': 'Extra Tools',
-    'summary': 'Father.',
+    'name': "website_sale_ext",
+
+    'summary':"""
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    
+    'description':"""
+        Long description of module's purpose
+    """,
+
+    'author': "OMy Company",
+    'website': "http://www.mycompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
     'depends': ['base'],
+
+    # always loaded
     'data': [
-              'views/views.xml',
-            ],
-    'images': ['static/description/banner.jpg'],
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml'
+    ],
+    
+    # only loaded in demostration mode
+
+    'demo': [
+        'demo/demo.xml',
+    ],    
 }
